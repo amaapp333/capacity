a184a0761382486e2d070fdc1bdd8460d34a6fb6
amaapp333 <118077998+amaapp333@users.noreply.github.com>
2022-11-21 04:03:16 +0200

    Create capacity.config.ts

---
+++ capacity.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.adball.news',
+  appName: 'My News App',
+  webDir: 'www',
+};
+
+export default config;
